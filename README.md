30c720f2d72ac6fbac1fc358074b5486a9065cb0
Anton Bugryakov <antis_amdg@mail.ru>
2020-12-27 22:52:01 +0300

    commit-1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/q5ln0ggos5feb8bm/branch/main?svg=true)](https://ci.appveyor.com/project/Antis85/ahj-forms-hw1/branch/main)
 # ahj-forms-hw1
+https://antis85.github.io/ahj-forms-hw1/
---
+++ e2e/e2e.test.js
@@ -0,0 +1,62 @@
+/*  eslint max-len: ["error", { "ignoreComments": true }] */
+import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe('Popover homework e2e tests', () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', () => {
+        reject();
+      });
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppeteer.launch({ // опции нужно закомментить перед пушем (для CI)
+      // headless: false, // show gui
+      // slowMo: 500,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test('should toggle "active" and "hidden" class', async () => {
+    await page.goto(baseUrl);
+
+    // await page.waitForSelector('[data-widget="popover-top"]', { visible: false, hidden: true });
+    // await page.waitForSelector('[data-widget="popover-top"].active', { visible: false, hidden: true });
+    await page.waitForSelector('[data-widget="popover-top"].hidden');
+    // await page.waitForSelector('[data-widget="popover-top"].hidden', { visible: true, hidden: false });
+
+    const button = await page.$('.btn');
+    button.click();
+
+    await page.waitForSelector('[data-widget="popover-top"].active');
+    await page.waitForSelector('[data-widget="popover-top"]');
+    // await page.waitForSelector('[data-widget="popover-top"]', { visible: true, hidden: false });
+    // await page.waitForSelector('[data-widget="popover-top"].hidden', { visible: false, hidden: true });
+
+    button.click();
+
+    // await page.waitForSelector('[data-widget="popover-top"].active', { visible: false, hidden: true });
+    await page.waitForSelector('[data-widget="popover-top"].hidden');
+    // await page.waitForSelector('[data-widget="popover-top"].hidden', { visible: true, hidden: false });
+    // await page.waitForSelector('[data-widget="popover-top"]', { visible: false, hidden: true });
+  });
+});
